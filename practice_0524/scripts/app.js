70d59f0ddf8bddf5d749a2aacfcf4d74b70ef521
psh <701tkdgns@inu.ac.kr>
2022-05-24 22:33:08 +0900

    0524

---
+++ practice_0524/scripts/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    // HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed: 1000,
+        navText : ['PREV','NEXT'],
+        responsive:{
+            0:{
+                nav:false
+            },
+            768:{
+                nav:true
+            }
+        }
+    })
+
+    // project slider
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        smartSpeed: 1000,
+        margin: 24,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                margin:0,
+            },
+            768:{
+                items:2,
+            },
+            1140:{
+                items: 2,
+                center: true,
+                dots:true,
+            }
+        }
+    })
+});
